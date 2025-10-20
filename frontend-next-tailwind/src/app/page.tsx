4e2e8f3ba0bbf9f97bd1aaa4f57ec1bf89581f30
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2025-10-20 15:53:04 +0530

    Update package dependencies, add PostCSS configuration, and enhance global styles
    - Updated package.json to include the latest version of Next.js and reorganized dependencies.
    - Added a new PostCSS configuration file for Tailwind CSS integration.
    - Enhanced global styles with a new border-image class for improved UI elements.
    - Refactored the Home component layout to include a HeroSection and streamline the structure.
    - Introduced multiple new SVG images for visual enhancements across the application.

--- frontend-next-tailwind/src/app/page.tsx
+++ frontend-next-tailwind/src/app/page.tsx
@@ -1,35 +1,18 @@
 import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative px-11">
-      <div className=" w-full h-screen ">
-        <Navbar />
-
-        {/* 1. Base color */}
-        {/* <div className="absolute inset-0 "></div> */}
-
-        {/* 2. Noise overlay */}
-        <div
-        // className="absolute inset-0 bg-[url('/images/noise.png')] bg-repeat"
-        // style={{
-        //   backgroundSize: "200px 200px",
-        //   opacity: 0.86,
-        //   mixBlendMode: "soft-light",
-        // }}
-        />
-
-        {/* 3. Gradient overlay (if needed) */}
-        {/* <div className="absolute inset-0 bg-gradient-to-b from-[#1B1B25]/80 to-[#65658B]/80"></div> */}
-
-        {/* 4. Content */}
-        <div className=" z-10 p-8 text-white">Your content here</div>
-      </div>
+    <div className="relative px-11  flex flex-col min-h-screen">
+      {/* <div className=" w-full  h-screen "> */}
+      <Navbar />
+      {/* <HeroSection />
+      <HeroSection /> */}
+      <HeroSection />
+      {/* </div> */}
       <Footer />
     </div>
   );
 }
-
-// background: radial-gradient(50% 50% at 50% 50%, #D9D9D9 0%, rgba(115, 115, 115, 0) 100%);
